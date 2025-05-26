8b0466d44572fdcc1fb3e9fd981b71f4d92188c4
GiovanniDMaio <giovannidimaiopalermo@gmail.com>
2025-05-26 17:06:53 +0200

    First update

--- Esercizi/Struct_Ex1.c
+++ Esercizi/Struct_Ex1.c
@@ -1,6 +1,15 @@
-// Esercizio n1 dell'esercitazione sulle strutture in C
-// Descrizione: Creare una struttura per rappresentare una pietanza con nome, numero di ingredienti,
-// ingredienti, dosi, tempo di cottura e tipo (primo, secondo, contorno, dolce).
+/*
+Esercizio 1
+Si implementi un programma che permetta di rappresentare attraverso una opportuna struct una pietanza.
+Di ogni pietanza conosciamo: il nome, gli ingredienti e le corrispondenti dosi (in g), il tempo di cottura, il tipo
+di piatto (antipasto, primo, secondo, contorno, dolce).
+Nel main, chiedere all’utente di inserire da tastiera i dati di una pietanza (lasciandogli scegliere il numero di
+ingredienti) e poi stampare i dati a schermo.
+Esempio: nome = pasta col pomodoro, {(pasta, 80g), (salsa, 40g), (parmigiano, 20g)}, 15 minuti, primo
+piatto. Oppure nome = cotoletta, {(carne, 140g), (uovo, 30g), (pan grattato, 30g), (sale, 5g), (olio, 20g)}, 7
+minuti, secondo piatto (notare che la lista degli ingredienti è variabile: si consideri che ci possano essere al più
+10 ingredienti).
+*/
 
 
 #include <stdio.h>
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Fondamenti_Programmazione
 
+Qui carico tutto ciò che ho fatto durante il corso di Fondamenti di Programmazione.
+
+- Esercizi svolti
+- Esempi di codice
